26acd6eaf60a4575e88bc7a89695d5fb8af0be84
ruanzerah <ruanlima2023@outlook.com>
2024-11-06 19:00:02 -0300

    feat: new data to collect from api

--- internal/api/api.go
+++ internal/api/api.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"encoding/json"
+	"errors"
 	"io"
 	"log"
 	"net/http"
@@ -10,16 +11,20 @@ import (
 	"github.com/ruanzerah/fw/internal"
 )
 
-func SendRequest(lat float32, long float32) *internal.WheaterResponse {
+func SendRequest(days int, lat, long float32) (*internal.WeatherResponse, error) {
+	if days > 16 {
+		return nil, errors.New("forecast_days must be lower than 16. Ex: (1, 3, 7, 16)")
+	}
 	latStr := strconv.FormatFloat(float64(lat), 'f', -1, 32)
 	longStr := strconv.FormatFloat(float64(long), 'f', -1, 32)
-	url := "https://api.open-meteo.com/v1/forecast?latitude=" + latStr + "&longitude=" + longStr + "&current=temperature_2m,weather_code&daily=uv_index_max&timezone=auto&forecast_days=1"
+	daysStr := strconv.FormatInt(int64(days), 10)
+	url := "https://api.open-meteo.com/v1/forecast?latitude=" + latStr + "&longitude=" + longStr + "&current=temperature_2m,apparent_temperature,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,uv_index_max&timezone=auto&forecast_days" + daysStr
 	res, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var r internal.WheaterResponse
+	var r internal.WeatherResponse
 	data, err := io.ReadAll(res.Body)
 	if err != nil {
 		log.Fatal(err)
@@ -30,5 +35,5 @@ func SendRequest(lat float32, long float32) *internal.WheaterResponse {
 		log.Fatal(err)
 	}
 
-	return &r
+	return &r, nil
 }
